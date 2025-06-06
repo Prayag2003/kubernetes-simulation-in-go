1f905d52517e5e771640cd662007c0c156a91516
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-06-06 12:38:11 +0530

    feat/main: flag to use hpa and configs

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,54 @@
 package main
 
 import (
+	"flag"
 	"time"
 
+	hpa "github.com/Prayag2003/kubernetes-simulation/internal/autoscaler"
+	"github.com/Prayag2003/kubernetes-simulation/internal/config"
+	"github.com/Prayag2003/kubernetes-simulation/internal/etcdstore"
 	"github.com/Prayag2003/kubernetes-simulation/internal/kubeapi"
+	"github.com/Prayag2003/kubernetes-simulation/internal/models"
 	"github.com/Prayag2003/kubernetes-simulation/internal/simulator"
 	"github.com/Prayag2003/kubernetes-simulation/utils"
 )
 
 func main() {
+	hpaConfigPath := flag.String("hpa-config", "", "Path to HPA config YAML file")
+	flag.Parse()
+
 	kube := kubeapi.NewKubeAPI()
 	utils.LogInfo("Main", "Starting workload simulation...")
 	simulator.StartWorkloadSim(kube)
 
+	// Load and apply HPA config
+	if *hpaConfigPath != "" {
+		cfg, err := config.LoadHPAConfigFromFile(*hpaConfigPath)
+		if err != nil {
+			utils.LogError("Main", "Failed to load HPA config: "+err.Error())
+		} else if cfg.Enabled {
+			utils.LogInfo("Main", "HPA config loaded. Starting autoscaler...")
+			hpa.StartHPA(kube, hpa.HPAConfig{
+				TargetCPU: cfg.TargetCPU,
+				MinPods:   cfg.MinPods,
+				MaxPods:   cfg.MaxPods,
+				Interval:  time.Duration(cfg.IntervalSeconds) * time.Second,
+			})
+		} else {
+			utils.LogWarn("Main", "HPA config disabled.")
+		}
+	}
+
+	// etcd test pod
+	dummyPod := models.Pod{ID: "demo-pod-1", Name: "hello-world"}
+	_ = etcdstore.GetStore().Set("/pods/demo-pod-1", dummyPod)
+
 	time.Sleep(30 * time.Second)
 
 	utils.LogWarn("Main", "Cleaning up all pods...")
-	for id := range kube.Pods {
-		kube.DeletePod(id)
+	for _, pod := range kube.ListPods() {
+		kube.DeletePod(pod.ID)
+		etcdstore.GetStore().Delete("/pods/" + pod.ID)
 	}
 
 	utils.LogSuccess("Main", "Simulation completed.")
