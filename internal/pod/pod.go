8432bd2e891dc35f1d674f1cf17aef841942d736
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-06-08 00:02:35 +0530

    feat/pods: update pod logic

--- internal/pod/pod.go
+++ internal/pod/pod.go
@@ -6,13 +6,17 @@ import (
 	"time"
 
 	"github.com/Prayag2003/kubernetes-simulation/internal/analytics"
+	"github.com/Prayag2003/kubernetes-simulation/internal/etcdstore"
 	"github.com/Prayag2003/kubernetes-simulation/internal/models"
 )
 
 func StartPod(pod *models.Pod, stopChan chan struct{}) {
 	analytics.Log("Pod", "info", "PodStart", fmt.Sprintf("Pod %s starting ...", pod.ID))
+
 	pod.Status = models.Running
 	pod.StartTime = time.Now()
+
+	_ = etcdstore.GetStore().Set("/pods/"+pod.ID, pod)
 
 	ticker := time.NewTicker(1 * time.Second)
 
@@ -21,19 +25,25 @@ func StartPod(pod *models.Pod, stopChan chan struct{}) {
 		case <-ticker.C:
 			cpu := rand.Float64()*100 + 100 // 100-200 millicores
 			mem := rand.Float64()*50 + 50   // 50-100 MB
+
 			pod.Resources = models.ResourceUsage{
 				CPU:    cpu,
 				Memory: mem,
 			}
 
-			analytics.Log("Pod", "info", "PodMetrics", fmt.Sprintf("Pod %s CPU: %.2fm, MEM: %.2fMB", pod.ID, cpu, mem))
 			log := fmt.Sprintf("[Pod %s] CPU: %.2fm, MEM: %.2fMB", pod.ID, cpu, mem)
 			pod.Logs = append(pod.Logs, log)
+
+			analytics.Log("Pod", "info", "PodMetrics", log)
+
+			_ = etcdstore.GetStore().Set("/pods/"+pod.ID, pod)
 
 		case <-stopChan:
 			ticker.Stop()
 			pod.Status = models.Succeeded
 			analytics.Log("Pod", "warn", "PodStop", fmt.Sprintf("Pod %s stopped.", pod.ID))
+
+			_ = etcdstore.GetStore().Set("/pods/"+pod.ID, pod)
 			return
 		}
 	}
